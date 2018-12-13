2c6fbe961cd6363f5f2444311de0c47075f991e5
kukuhbr <kukuhis777@gmail.com>
2018-12-14 00:49:33 +0700

    add fuction calculate sum distance

--- app/src/main/java/com/example/asus/imageconvolution/ControlPoint.java
+++ app/src/main/java/com/example/asus/imageconvolution/ControlPoint.java
@@ -5,6 +5,7 @@ import android.graphics.Point;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
 /**
  * Created by Winarto on 12/13/2018.
@@ -23,5 +24,21 @@ public class ControlPoint {
             tempBitmap.setPixel(offsetX+points[i].x, offsetY+points[i].y, color);
         }
         return tempBitmap;
+    }
+
+    public boolean validateSumDistance(ControlPoint c) {
+        float sum = calculateSumDistance(c);
+        if (sum < 40) {
+            return true;
+        } else
+            return false;
+    }
+
+    public float calculateSumDistance(ControlPoint c) {
+        float sum = 0;
+        for(int i = 0; i < points.length; i++) {
+            sum += Math.sqrt(Math.pow(points[i].x - c.points[i].x, 2)+Math.pow(Math.pow(points[i].y - c.points[i].y , 2));
+        }
+        return sum;
     }
 }
