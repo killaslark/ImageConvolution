acdb949430e989978b9171bca2eafe19093ea396
Winarto <13515061@std.stei.itb.ac.id>
2018-12-14 08:35:41 +0700

    Get control point and draw

--- app/src/main/java/com/example/asus/imageconvolution/Box.java
+++ app/src/main/java/com/example/asus/imageconvolution/Box.java
@@ -2,7 +2,13 @@ package com.example.asus.imageconvolution;
 
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.Point;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.abs;
 
 /**
  * Created by Winarto on 11/23/2018.
@@ -14,6 +20,8 @@ public class Box {
     public float top;
     public float bottom;
     public float size;
+    private ControlPoint edges;
+    final private int[][] nbrs = {{0, -1}, {1, -1}, {1, 0}, {1, 1}, {0, 1} ,{-1, 1} ,{-1, 0}, {-1, -1}, {0, -1}};
 
     public Box(){
         left = -1;
@@ -82,4 +90,60 @@ public class Box {
     public float getSize(){
         return (right-left)*(bottom-top);
     }
+
+    public ControlPoint getEdges(Bitmap bitmap, int offsetX, int offsetY){
+        if(edges == null){
+            Log.d("CONTORLPOINT", "INSIDE");
+            edges = getControlPoint(bitmap, offsetX, offsetY);
+        }
+        return edges;
+    }
+
+    private ControlPoint getControlPoint(Bitmap bitmap, int offsetX, int offsetY){
+        ControlPoint controlPoint = new ControlPoint();
+        int left = offsetX+(int)this.left;
+        int right = offsetX+(int)this.right;
+        int top = offsetY+(int)this.top;
+        int bottom = offsetY+(int)this.bottom;
+        int halfVertical = (int)(this.top+((this.bottom-this.top)/2));
+        int halfHorizontal = (int)(this.left+((this.right-this.left)/2));
+        Log.d("TOPCOORDINATE", "" + this.top);
+        Log.d("BOTTOMCOORDINATE", "" + this.bottom);
+        Log.d("HALFCOORDINATE", "" + halfVertical);
+        Point centerPoint = new Point(offsetX+halfHorizontal, offsetY+halfVertical);
+        Point topLeft = getFirstWhitePixel(bitmap, centerPoint, new Point(left, top), offsetX, offsetY);
+        Point topRight = getFirstWhitePixel(bitmap, centerPoint, new Point(right, top), offsetX, offsetY);
+        Point bottomLeft = getFirstWhitePixel(bitmap, centerPoint, new Point(left, bottom), offsetX, offsetY);
+        Point bottomRight = getFirstWhitePixel(bitmap, centerPoint, new Point(right, bottom), offsetX, offsetY);
+        controlPoint.points[0] = new Point((int)this.left, halfVertical);
+        controlPoint.points[1] = topLeft;
+        controlPoint.points[2] = new Point(halfHorizontal,(int)this.top);
+        controlPoint.points[3] = topRight;
+        controlPoint.points[4] = new Point((int)this.right, halfVertical);
+        controlPoint.points[5] = bottomRight;
+        controlPoint.points[6] = new Point(halfHorizontal,(int)this.bottom);
+        controlPoint.points[7] = bottomLeft;
+        return controlPoint;
+    }
+
+    private Point getFirstWhitePixel(Bitmap bitmap, Point from, Point to, int offsetX, int offsetY){
+        int x0 = from.x; int x1 = to.x;
+        int y0 = from.y; int y1 = to.y;
+        int dx = abs(x1-x0), sx = x0<x1 ? 1 : -1;
+        int dy = abs(y1-y0), sy = y0<y1 ? 1 : -1;
+        int err = (dx>dy ? dx : -dy)/2, e2;
+
+        for(;;){
+            int color = bitmap.getPixel(x0,y0);
+            if(color == Color.WHITE)
+                break;
+            else if (x0==x1 && y0==y1)
+                break;
+            e2 = err;
+            if (e2 >-dx) { err -= dy; x0 += sx; }
+            if (e2 < dy) { err += dx; y0 += sy; }
+        }
+        return new Point(x0-offsetX,y0-offsetY);
+    }
+
 }
